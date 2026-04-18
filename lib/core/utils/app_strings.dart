a37a17727ebe8f2fb0bb6837350b9c21bfee7d57
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-18 03:38:09 +0200

    Add app strings for localization support

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,11 @@
+const String appName = 'CashierZ';
+
+const String brandName = 'ابو النكد';
+const String cashireMode = 'وضع الكاشير';
+const String mangeProductsMode = 'ادارة المنتجات';
+const String titleFieldScan = 'امسح الباركود هنا...';
+const String productBasket = 'سلة المشتريات';
+const String listProduct = 'قائمة المنتجات';
+const String printInvoice = 'طباعة الفاتورة';
+const String totalPrice = 'السعر الكلي';  
+const String clearBasketButton = 'تفريغ السلة';  
