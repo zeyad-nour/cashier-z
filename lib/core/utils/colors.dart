810a719307bbedf35c3eaef911c3e6636c34c567
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-18 03:20:00 +0200

    Refactor: Remove unnecessary comments in main.dart and add colors.dart for color constants

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF2563EB);
+const Color primaryLight = Color(0xFFDBEAFE);
+const Color backgroundColor = Color(0xFFF8FAFC);
+const Color cardsAndContainers = Color(0xFFFFFFFF);
+const Color borderAndDivider = Color(0xFFE5E7EB);
+const Color primaryTextColor = Color(0xFF1F2937);
+const Color secounderyTextColor = Color(0xFF6B7280);
+const Color iconColor = Color(0xFF9CA3AF);
+const Color colorButtonSelectedTap = Color(0xff3B82F6);
