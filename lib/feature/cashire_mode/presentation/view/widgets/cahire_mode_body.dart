5bfa07713a5ccf68b53f62c1c28f8179ade2fd30
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-19 17:11:16 +0200

    Refactor CashireModeBody: remove unused imports and clean up code structure; update generated plugin files

--- lib/feature/cashire_mode/presentation/view/widgets/cahire_mode_body.dart
+++ lib/feature/cashire_mode/presentation/view/widgets/cahire_mode_body.dart
@@ -1,14 +1,13 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:cashier_z/core/widgets/custom_app_bar.dart';
-import 'package:cashier_z/core/widgets/scan_widget.dart';
+
 import 'package:cashier_z/feature/cashire_mode/presentation/view/widgets/cashire_view.dart';
-import 'package:cashier_z/feature/cashire_mode/presentation/view/widgets/options_widget.dart';
+
 import 'package:cashier_z/core/widgets/select_mode.dart';
-import 'package:cashier_z/feature/mange_products_mode/presentation/view/mange_products_mode.dart';
+
 import 'package:cashier_z/feature/mange_products_mode/presentation/view/widgets/mange_products_mode_body.dart';
 import 'package:flutter/material.dart';
-import 'package:cashier_z/feature/cashire_mode/presentation/view/widgets/cashire_view.dart';
 
 class CashireModeBody extends StatefulWidget {
   const CashireModeBody({super.key});
@@ -37,7 +36,6 @@ class _CashireModeBodyState extends State<CashireModeBody> {
 
         const SizedBox(height: 10),
 
-  
         Expanded(
           child: AnimatedSwitcher(
             duration: const Duration(milliseconds: 300),
